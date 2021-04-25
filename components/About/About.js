5988d80227606181b5a3c9a8ae69cf6ceaa32b4f
Vithusan <thavarajah.vithusan@gmail.com>
2021-04-25 11:52:36 -0400

    added typed.js

--- components/About/About.js
+++ components/About/About.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import Image from 'next/image'
+import Typing from './Typing'
 
 function About () {
+    var options = {
+        string: ['<i>First</i> sentence.', '&mp; a second sentence.'],
+        typeSpeed: 30
+    };
     return (
         <div className ='hero-container'>
-            <Image src='/images/CutOut.png' height="400px" width="225px"></Image>
-            <h1>WELCOME!</h1>
-            <p>I'm Vithusan Thavarajah</p>
+            <Image src='/images/CutOut.png' preload="true" height="400px" width="225px"></Image>
+            <h1>Hi, I'm Vithusan!</h1>
+            {/* <p>I'm Vithusan Thavarajah</p> */}
+            <p>
+                <Typing/>
+            </p>
         </div>
 
     )
---
+++ components/About/Typing.js
@@ -0,0 +1,35 @@
+import React from "react";
+import Typed from 'typed.js'
+// Import the desired words
+const words = ['Software Engineer.', 'Full Stack Web Developer.', 'Photographer.']
+class Typing extends React.Component {
+  componentDidMount() {
+    const options = {
+      strings: words,
+      typeSpeed: 50,
+      backSpeed: 50,
+      loop: true,
+      cursorChar: "|",
+    };
+    // this.el refers to the <span> in the render() method
+    this.typed = new Typed(this.el, options);
+  }
+componentWillUnmount() {
+    // Please don't forget to cleanup animation layer
+    this.typed.destroy();
+  }
+  
+  render() {
+    return (
+      <>
+        <span
+          style={{ whiteSpace: "pre" }}
+          ref={(el) => {
+            this.el = el;
+          }}
+        />
+      </>
+    );
+  }
+}
+export default Typing;
