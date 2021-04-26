591fc737d95c78942bdc5936824b44f130acd2cb
Vithusan Thavarajah <vithusan@Vithusans-Work-MacBook-Pro.local>
2021-04-26 00:05:00 -0400

    updated about bio

--- components/About/About.js
+++ components/About/About.js
@@ -10,7 +10,7 @@ function About () {
         <div id='About' className ='hero-container'>
             <h1></h1>
             {/* <p>I'm Vithusan Thavarajah</p> */}
-            <p>I JUST WANT TO BE NOTICED BY CHAD SCOTT BRO :(</p>
+            <p>I'm currently a Software Developer at TD Bank in Toronto. I graduated from the University of Toronto with an Honours Bachelor of Science where I studied Computer Science and specialized in Machine Learning. When I’m not in front of my computer, I’m most likely carrying my camera around and fulfilling my love for photography!</p>
         </div>
 
     )
